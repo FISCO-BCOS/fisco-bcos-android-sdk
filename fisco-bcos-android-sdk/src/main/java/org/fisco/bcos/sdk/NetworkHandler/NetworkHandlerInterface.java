5d2f6203b3d383b7c74e56ee5a1080d8a5588e46
chaychen2005 <35914409+chaychen2005@users.noreply.github.com>
2021-02-20 15:34:53 +0800

    delete network related module (#17)

--- fisco-bcos-android-sdk/src/main/java/org/fisco/bcos/sdk/NetworkHandler/NetworkHandlerInterface.java
+++
@@ -1,8 +0,0 @@
-package org.fisco.bcos.sdk.NetworkHandler;
-
-public interface NetworkHandlerInterface {
-
-    void setIpAndPort(String ipPort);
-
-    String onRPCRequest(String requestBodyJsonStr);
-}
