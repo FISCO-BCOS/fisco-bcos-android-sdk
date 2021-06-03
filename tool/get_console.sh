b94a298b11e1b343bc48aa1b97c62863e13f94ec
longJJ2016 <1017303686@qq.com>
2021-06-03 14:29:53 +0800

    修复 控制台升级后javasdk找不到无法删除的问题 (#31)

--- tool/get_console.sh
+++ tool/get_console.sh
@@ -4,7 +4,7 @@
 download_console() {
 curl -#LO https://github.com/FISCO-BCOS/console/releases/download/v2.7.1/download_console.sh
 bash download_console.sh
-rm -rf console/lib/fisco-bcos-java-sdk-2.7.1.jar
+find console/lib/fisco-bcos-java-sdk* -exec rm -rf {} \;
 cp fisco-bcos-java-sdk-2.7.2-SNAPSHOT.jar console/lib/ 
 }
 
